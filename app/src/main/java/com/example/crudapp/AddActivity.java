6998f8ba36c1fa78bf381caf18df542cb16477ca
MasaSaranovic <masasaranovic@gmail.com>
2024-12-03 19:54:35 +0100

    Vezbe 8

---
+++ app/src/main/java/com/example/crudapp/AddActivity.java
@@ -0,0 +1,89 @@
+package com.example.crudapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText name, email, course, surl;
+    Button btnAdd, btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        name = (EditText)findViewById(R.id.txtName);
+        course = (EditText)findViewById(R.id.txtCourse);
+        email = (EditText)findViewById(R.id.txtEmail);
+        surl = (EditText)findViewById(R.id.txtImageUrl);
+
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnBack = (Button)findViewById(R.id.btnBack);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertData();
+                clearAll();
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void insertData(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", name.getText().toString());
+        map.put("course", course.getText().toString());
+        map.put("email", email.getText().toString());
+        map.put("surl", surl.getText().toString());
+
+        FirebaseDatabase.getInstance().getReference().child("students").push()
+                .setValue(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(AddActivity.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(AddActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+
+                        finish();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(AddActivity.this, "Error while Inserting", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+    private void clearAll(){
+        name.setText("");
+        email.setText("");
+        course.setText("");
+        surl.setText("");
+    }
+}
--- app/src/main/java/com/example/crudapp/MainActivity.java
+++ app/src/main/java/com/example/crudapp/MainActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.SearchView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     MainAdapter mainAdapter;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
 
         mainAdapter = new MainAdapter(options);
         recyclerView.setAdapter(mainAdapter);
+
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.floatingActionButton);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), AddActivity.class));
+            }
+        });
     }
     @Override
     protected void onStart() {
